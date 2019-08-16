34cd810ccc6f95cb91be1ea9d1afd14dfa8a0092
K Hong <albtrossfacefall@gmail.com>
2019-08-16 19:48:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(test_integer)
+  bypass_t=[-2,2]
+  bypass_t.each {|i| return true if test_integer==i}
+  
+  bypass_f=(-1..1)
+  bypass_f.each {|i| return false if test_integer==i}
+  
+  (2...test_integer.abs).none? {|i| test_integer%i==0}
+end
