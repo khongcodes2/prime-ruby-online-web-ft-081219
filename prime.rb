652ad61f448d78de35b0d68d88b797454ca64a8f
K Hong <albtrossfacefall@gmail.com>
2019-08-16 19:38:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(test_integer)
-  if test_integer == 2
-    return true
-  end
+  bypass=[-2,-1,1,2]
+  return true if test_integer.any?
+  
   
   (2...test_integer).none? {|i| test_integer%i==0}
 end
