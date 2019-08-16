3562f835fe073e8279f65232c373de57454df135
K Hong <albtrossfacefall@gmail.com>
2019-08-16 19:47:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 def prime?(test_integer)
-  bypass=[-2,2]
-  bypass.each do |i|
+  bypass_t=[-2,2]
+  bypass_t.each do |i|
     if test_integer==i
       return true
     end
   end
+  
+  bypass_f=(-1..1)
+  bypass_f.each {|i| return false if test_integer==i}
   
   (2...test_integer.abs).none? {|i| test_integer%i==0}
 end
